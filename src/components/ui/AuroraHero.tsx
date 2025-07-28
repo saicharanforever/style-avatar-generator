bf64cf5b3be1bfb3a04bc1cbbdb034a9da10f398
saicharanforever <saicharanvarkala192@gmail.com>
2025-07-28 22:07:07 +0530

    Update AuroraHero.tsx

--- src/components/ui/AuroraHero.tsx
+++ src/components/ui/AuroraHero.tsx
@@ -126,7 +126,7 @@ export const AuroraHero = () => {
 
         {/* Right Video - White background instead of black sides */}
         <div className="w-2/5 p-4 flex justify-center items-center">
-          <div className="relative rounded-2xl overflow-hidden shadow-2xl w-80 h-[60vh] max-h-[500px] bg-white">
+          <div className="relative rounded-2xl overflow-hidden shadow-2xl w-80 h-[60vh] max-h-[500px]" style={{ backgroundColor: 'white' }}>
             <iframe
               src="https://player.cloudinary.com/embed/?cloud_name=dtealftsb&public_id=Untitled_design_12_mbyezr&profile=cld-default&autoplay=true&loop=true&muted=true&controls=false"
               className="w-full h-full"
