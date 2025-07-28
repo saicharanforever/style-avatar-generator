d86f102ff474e5ebbd5cb1b8bdf399d13023e3e4
saicharanforever <saicharanvarkala192@gmail.com>
2025-07-28 22:55:49 +0530

    Update TypingAnimation.tsx

--- src/components/ui/TypingAnimation.tsx
+++ src/components/ui/TypingAnimation.tsx
@@ -10,7 +10,7 @@ interface TypingAnimationProps {
 export const TypingAnimation: React.FC<TypingAnimationProps> = ({
   text,
   className = '',
-  speed = 100,
+  speed = 500,
   delay = 0
 }) => {
   const [displayedText, setDisplayedText] = useState('');
